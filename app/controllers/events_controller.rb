3846227a782541fb4bcf1fea9874431d3326c6d0
lycho33 <cho.lyds@gmail.com>
2021-09-09 22:46:41 -0400

    updated events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,35 +8,35 @@ class EventsController < ApplicationController
     render json: @events
   end
 
-  # # GET /events/1
-  # def show
-  #   render json: @event
-  # end
+  # GET /events/1
+  def show
+    render json: @event
+  end
 
-  # # POST /events
-  # def create
-  #   @event = Event.new(event_params)
+  # POST /events
+  def create
+    @event = Event.new(event_params)
 
-  #   if @event.save
-  #     render json: @event, status: :created, location: @event
-  #   else
-  #     render json: @event.errors, status: :unprocessable_entity
-  #   end
-  # end
+    if @event.save
+      render json: @event, status: :created, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
 
-  # # PATCH/PUT /events/1
-  # def update
-  #   if @event.update(event_params)
-  #     render json: @event
-  #   else
-  #     render json: @event.errors, status: :unprocessable_entity
-  #   end
-  # end
+  # PATCH/PUT /events/1
+  def update
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
 
-  # # DELETE /events/1
-  # def destroy
-  #   @event.destroy
-  # end
+  # DELETE /events/1
+  def destroy
+    @event.destroy
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -46,6 +46,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:name, :date, :location, :further_info, :deadline, :type, :teacher_id)
+      params.require(:event).permit(:name, :date, :city, :state, :country, :further_info, :deadline, :type, :teacher_id)
     end
 end
